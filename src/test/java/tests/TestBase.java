d8b13994b40e356b8248e402791de33e740765a9
Anastasiya Elkafrawi <atverdova78@gmail.com>
2025-08-06 20:39:12 +0400

    Change browser to FireFox

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,20 +14,20 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
 
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--incognito");
+        options.addArguments("--incognito");
 //        if (!System.getenv().containsKey("OS") || !System.getenv("OS").equals("Windows_NT")) {
 //            options.addArguments("--user-data-dir=/home/jenkins/workspace/anastasiya_elkafrawy_11_lesson_qaguru/chrome_data");
 //        }
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--headless=new");
-//        options.addArguments("--disable-gpu");
-//        options.addArguments("--disable-extensions");
-//        options.addArguments("--disable-setuid-sandbox");
-//
-//        options.addArguments("--disable-application-cache");
-//        options.addArguments("--enable-do-not-track");
-//        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-setuid-sandbox");
+
+        options.addArguments("--disable-application-cache");
+        options.addArguments("--enable-do-not-track");
+        options.addArguments("--disable-popup-blocking");
 
         Configuration.browserCapabilities = options;
     }
