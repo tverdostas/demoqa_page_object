9484e6dae2a22a19211999b327bb707c46a22caf
Anastasiya Elkafrawi <atverdova78@gmail.com>
2025-08-06 20:27:23 +0400

    Change browser to FireFox

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,13 +15,19 @@ public class TestBase {
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
-        if (!System.getenv().containsKey("OS") || !System.getenv("OS").equals("Windows_NT")) {
-            options.addArguments("--user-data-dir=/home/jenkins/workspace/anastasiya_elkafrawy_11_lesson_qaguru/chrome_data");
-        }
-        options.addArguments("--disable-extensions");
+//        if (!System.getenv().containsKey("OS") || !System.getenv("OS").equals("Windows_NT")) {
+//            options.addArguments("--user-data-dir=/home/jenkins/workspace/anastasiya_elkafrawy_11_lesson_qaguru/chrome_data");
+//        }
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless=new");
         options.addArguments("--disable-gpu");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-setuid-sandbox");
+
+        options.addArguments("--disable-application-cache");
+        options.addArguments("--enable-do-not-track");
+        options.addArguments("--disable-popup-blocking");
 
         Configuration.browserCapabilities = options;
     }
