2fb1c7d48ccc63fbf1e0921633ca2ec610133b35
Anastasiya Elkafrawi <atverdova78@gmail.com>
2025-08-06 20:11:09 +0400

    Change browser to FireFox

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,7 +15,7 @@ public class TestBase {
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
-        if (!System.getenv("OS").equals("Windows_NT")) {
+        if (!System.getenv().containsKey("OS") || !System.getenv("OS").equals("Windows_NT")) {
             options.addArguments("--user-data-dir=/home/jenkins/workspace/anastasiya_elkafrawy_11_lesson_qaguru/chrome_data");
         }
         options.addArguments("--disable-extensions");
