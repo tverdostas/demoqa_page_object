aa1e6c0dc1ef5c253d5e70eb6a5b3f96de2ee454
Anastasiya Elkafrawi <atverdova78@gmail.com>
2025-08-06 19:54:36 +0400

    Change browser to FireFox

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -13,7 +13,7 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         Configuration.browser = "firefox";
-        Configuration.browserVersion = "130.0";
+        // Configuration.browserVersion = "134.0";
 
 //        ChromeOptions option = new ChromeOptions();
 //        option.addArguments("--incognito");
